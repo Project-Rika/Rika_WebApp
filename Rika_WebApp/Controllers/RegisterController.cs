1ad72b367b8c5e0b449288509db8d1753aa9363b
Mariam <mariam.al.it@hotmail.com>
2024-11-08 08:20:01 +0100

    Created Register Controller

---
+++ Rika_WebApp/Controllers/RegisterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rika_WebApp.Controllers;
+
+public class RegisterController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Rika_WebApp/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rika_WebApp.ViewModels;
+
+public class RegistrationViewModel
+{
+    [Display(Name = "First Name")]
+    [Required(ErrorMessage = "First name is required")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Last Name")]
+    [Required(ErrorMessage = "Last name is required")]
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "E-mail is required")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [Display(Name = "Confirm Password")]
+    [Required(ErrorMessage = "Confirm password is required")]
+    [Compare(nameof(Password), ErrorMessage = "Passwords don't match")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; } = null!;
+}
